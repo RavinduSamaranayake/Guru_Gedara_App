7cdbc83efd267f52518c75fa3acc080a544e57e1
RavinduSamaranayake <ovkravindu95@gmail.com>
2018-11-07 23:50:18 +0530

    add to the loading gif

--- app/src/main/java/com/example/user/guruforstudent/WaitingReg.java
+++ app/src/main/java/com/example/user/guruforstudent/WaitingReg.java
@@ -2,6 +2,9 @@ package com.example.user.guruforstudent;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.WindowManager;
+
+import com.example.user.guruforstudent.Controls.GifImageView;
 
 public class WaitingReg extends AppCompatActivity {
 
@@ -9,5 +12,11 @@ public class WaitingReg extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.waiting);
+        getSupportActionBar().hide();
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        GifImageView gifImageView = (GifImageView) findViewById(R.id.GifImageView);
+        gifImageView.setGifImageResource(R.drawable.waitreggif);
     }
 }
+
+
