7c19a4307247a950c9b59d01b73b517c6df25190
RavinduSamaranayake <ovkravindu95@gmail.com>
2019-01-05 10:48:36 +0530

    complete rate functinality

--- app/src/main/java/com/example/user/guruforstudent/viewComments.java
+++ app/src/main/java/com/example/user/guruforstudent/viewComments.java
@@ -1,11 +1,17 @@
 package com.example.user.guruforstudent;
 
+import android.app.Activity;
+import android.app.Dialog;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ListView;
+import android.widget.RatingBar;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.user.guruforstudent.Models.Course;
@@ -34,14 +40,65 @@ public class viewComments extends AppCompatActivity {
         addrevbtn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-              addNewReviewPg(stdid,insId);
+              addNewReviewPg(viewComments.this,stdid,insId);
             }
         });
 
 
     }
 
-    private void addNewReviewPg(int stdid, int insId) {   //to load to the add review custom layout
+    private void addNewReviewPg(Activity activity, int stdid, int insId) {   //to load to the add review custom layout
+        Dialog dialog =new Dialog(activity);
+        dialog.setContentView(R.layout.ratings);
+        dialog.setTitle("Review Institute");
+        final RatingBar mRatingBar  = (RatingBar) findViewById(R.id.ratingBar);
+        final TextView mRatingScale = (TextView) findViewById(R.id.tvRatingScale);
+        final EditText mFeedback = (EditText) findViewById(R.id.etFeedback);
+        final Button mSendFeedback = (Button) findViewById(R.id.btnSubmit);
+        //set width of dialog
+        int width =(int)(activity.getResources().getDisplayMetrics().widthPixels*0.95);
+        //set high of dialog
+        int high =(int)(activity.getResources().getDisplayMetrics().widthPixels*1);
+        dialog.getWindow().setLayout(width,high);
+        dialog.show();
+        mRatingBar.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
+            @Override
+            public void onRatingChanged(RatingBar ratingBar, float v, boolean b) {
+                mRatingScale.setText(String.valueOf(v));
+                switch ((int) ratingBar.getRating()) {
+                    case 1:
+                        mRatingScale.setText("Very bad");
+                        break;
+                    case 2:
+                        mRatingScale.setText("Need some improvement");
+                        break;
+                    case 3:
+                        mRatingScale.setText("Good");
+                        break;
+                    case 4:
+                        mRatingScale.setText("Great");
+                        break;
+                    case 5:
+                        mRatingScale.setText("Excellent");
+                        break;
+                    default:
+                        mRatingScale.setText("");
+                }
+            }
+        });
+
+        mSendFeedback.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (mFeedback.getText().toString().isEmpty()) {
+                    Toast.makeText(viewComments.this, "Please fill in feedback text box", Toast.LENGTH_LONG).show();
+                } else {
+                    mFeedback.setText("");
+                    mRatingBar.setRating(0);
+                    Toast.makeText(viewComments.this, "Thank you for sharing your feedback", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
 
     }
 }
